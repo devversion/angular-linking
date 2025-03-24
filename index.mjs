4609096b6b8a049173a0c10165f7674bad7508ea
Paul Gschwendtner <paulgschwendtner@gmail.com>
2025-03-24 15:11:57 +0000

    Disable Babel looking for config files

--- index.mjs
+++ index.mjs
@@ -1,19 +1,27 @@
-import linkerBabelPlugin from '@angular/compiler-cli/linker/babel';
-import {transformAsync} from '@babel/core';
-import assert from 'node:assert';
-import {readFile, writeFile} from 'node:fs/promises';
-import {globSync} from 'tinyglobby';
+import linkerBabelPlugin from "@angular/compiler-cli/linker/babel";
+import { transformAsync } from "@babel/core";
+import assert from "node:assert";
+import { readFile, writeFile } from "node:fs/promises";
+import { globSync } from "tinyglobby";
 
 async function main() {
-  const fesmBundles = globSync('fesm2022/**/*.mjs');
+  const fesmBundles = globSync("fesm2022/**/*.mjs");
   const tasks = [];
-  const babelOptions = {plugins: [linkerBabelPlugin, {}]};
+  /** @type {import('@babel/core').TransformOptions} */
+  const babelOptions = {
+    plugins: [[linkerBabelPlugin, {}]],
+    configFile: false,
+    babelrc: false,
+  };
 
   for (const bundleFile of fesmBundles) {
     tasks.push(
       (async () => {
-        const content = await readFile(bundleFile, 'utf8');
-        const result = await transformAsync(content, {...babelOptions, filename: bundleFile});
+        const content = await readFile(bundleFile, "utf8");
+        const result = await transformAsync(content, {
+          ...babelOptions,
+          filename: bundleFile,
+        });
 
         await writeFile(`${bundleFile}.linked.js`, result.code);
       })(),
@@ -22,12 +30,12 @@ async function main() {
 
   tasks.push(
     (async () => {
-      const packageJsonRaw = await readFile('package.json', 'utf8');
+      const packageJsonRaw = await readFile("package.json", "utf8");
       const packageJson = JSON.parse(packageJsonRaw);
 
       assert(
         packageJson.exports,
-        'No `package.json` `exports` for package. Cannot link Angular code',
+        "No `package.json` `exports` for package. Cannot link Angular code",
       );
 
       for (const conditions of Object.values(packageJson.exports)) {
@@ -35,17 +43,17 @@ async function main() {
         if (!defaultCondition || !/fesm2022/.test(defaultCondition)) {
           continue;
         }
-        conditions['ng-linked'] = `${defaultCondition}.linked.js`;
+        conditions["ng-linked"] = `${defaultCondition}.linked.js`;
       }
 
-      await writeFile('package.json', JSON.stringify(packageJson, null, 2));
+      await writeFile("package.json", JSON.stringify(packageJson, null, 2));
     })(),
   );
 
   await Promise.all(tasks);
 }
 
-main().catch(e => {
+main().catch((e) => {
   console.error(e);
   process.exitCode = 1;
 });
